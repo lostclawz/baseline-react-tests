26c0451dbf6e9ef7cd9d21ab4a7644fe954f995b
Philip Kuperberg <philipkuperberg@gmail.com>
2018-12-16 15:27:31 -0500

    added a test for mocha

---
+++ src/tests/test-setup.js
@@ -0,0 +1,9 @@
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
+import React from 'react';
+
+describe('mocha', function(){
+   it(`can run tests`, () => {
+      expect(2 + 2).to.equal(4);
+   })
+});
